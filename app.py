bedc4fc35bd06586d885e6b171db1061ddf42dc4
Devisree Raj <72488986+devisreeraj@users.noreply.github.com>
2022-06-12 12:53:44 +0530

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Starting machine Learning Project with CICD Pipeline"
+    return "Starting machine Learning Project with CI-CD Pipeline"
 
 if __name__ == "__main__":
     app.run(debug=True)    
